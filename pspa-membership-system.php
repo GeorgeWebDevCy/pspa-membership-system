b818368b30e972507b3dc6446a818331965bd9bf
George Nicolaou <orionas.elite@gmail.com>
2025-09-14 09:53:50 +0300

    style login by details form and bump version

--- pspa-membership-system.php
+++ pspa-membership-system.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: PSPA Membership System
  * Description: Membership system for PSPA.
- * Version: 0.0.55
+ * Version: 0.0.56
  * Author: George Nicolaou
  * Author URI: https://profiles.wordpress.org/orionaselite/
  *
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'PSPA_MS_VERSION', '0.0.55' );
+define( 'PSPA_MS_VERSION', '0.0.56' );
 
 define( 'PSPA_MS_LOG_FILE', plugin_dir_path( __FILE__ ) . 'pspa-ms.log' );
 
