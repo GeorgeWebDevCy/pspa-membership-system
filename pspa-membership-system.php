60426261c0fabda9c35fe966023b4f22389cff1b
George Nicolaou <orionas.elite@gmail.com>
2025-09-17 18:47:05 +0300

    Align graduate profile tabs with Woo account menu

--- pspa-membership-system.php
+++ pspa-membership-system.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: PSPA Membership System
  * Description: Membership system for PSPA.
- * Version: 0.0.111
+ * Version: 0.0.112
  * Author: George Nicolaou
  * Author URI: https://profiles.wordpress.org/orionaselite/
  *
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'PSPA_MS_VERSION', '0.0.111' );
+define( 'PSPA_MS_VERSION', '0.0.112' );
 
 if ( ! defined( 'PSPA_MS_ENABLE_LOGGING' ) ) {
     define( 'PSPA_MS_ENABLE_LOGGING', defined( 'WP_DEBUG' ) && WP_DEBUG );
