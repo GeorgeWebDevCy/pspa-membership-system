c20a2141a581b697085c480ea2010e61183bc5d7
George Nicolaou <orionas.elite@gmail.com>
2025-09-19 20:53:30 +0300

    Reverse profile header gradient

--- pspa-membership-system.php
+++ pspa-membership-system.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: PSPA Membership System
  * Description: Membership system for PSPA.
- * Version: 0.0.121
+ * Version: 0.0.122
  * Author: George Nicolaou
  * Author URI: https://profiles.wordpress.org/orionaselite/
  *
@@ -14,7 +14,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-define( 'PSPA_MS_VERSION', '0.0.121' );
+define( 'PSPA_MS_VERSION', '0.0.122' );
 
 if ( ! defined( 'PSPA_MS_ENABLE_LOGGING' ) ) {
     define( 'PSPA_MS_ENABLE_LOGGING', defined( 'WP_DEBUG' ) && WP_DEBUG );
